4b78076bd1c90abd69866d54edf43c284d1d3eee
RickFqt <henriquefouquet@gmail.com>
2024-05-09 00:23:44 -0300

    Final cleanup

--- trab1/tests/buffers/buffer1.cpp
+++ trab1/tests/buffers/buffer1.cpp
@@ -4,24 +4,27 @@
 
 int sc_main(int argc, char* argv[]) {
 
-    sc_signal<sc_uint<5>> next_instruction_adress_in_sig;
     sc_signal<sc_uint<32>> instruction_data_in_sig;
+	sc_signal<sc_uint<6>> opcode_out_sig;
+	sc_signal<sc_uint<5>> rs_out_sig;
+	sc_signal<sc_uint<5>> rt_out_sig;
+	sc_signal<sc_uint<5>> rd_out_sig;
+	sc_signal<sc_uint<16>> rdShamtFunct_out_sig;
 
-    sc_signal<sc_uint<5>> next_instruction_adress_out_sig; // Maybe change this size
-    sc_signal<sc_uint<32>> instruction_data_out_sig;
 
 	sc_clock TestClk("TestClock", 10, SC_NS, 0.5);
 
 	buffer1_testbench Tb("Testbench");
-	Tb.next_instruction_adress_in(next_instruction_adress_in_sig);
 	Tb.instruction_data_in(instruction_data_in_sig);
 	Tb.Clk(TestClk);
 
 	buffer_1 b1("buffer1");
-	b1.next_instruction_adress_in(next_instruction_adress_in_sig);
 	b1.instruction_data_in(instruction_data_in_sig);
-	b1.next_instruction_adress_out(next_instruction_adress_out_sig);
-	b1.instruction_data_out(instruction_data_out_sig);
+	b1.opcode_out(opcode_out_sig);
+	b1.rs_out(rs_out_sig);
+	b1.rt_out(rt_out_sig);
+	b1.rd_out(rd_out_sig);
+	b1.rdShamtFunct_out(rdShamtFunct_out_sig);
     b1.clk(TestClk);
 
 	//========================= waveform
@@ -29,10 +32,12 @@ int sc_main(int argc, char* argv[]) {
 	fp=sc_create_vcd_trace_file("wave");
 	fp->set_time_unit(1, sc_core::SC_NS);
 	sc_trace(fp, TestClk, "Clock");
-	sc_trace(fp, next_instruction_adress_in_sig, "adress_in");
 	sc_trace(fp, instruction_data_in_sig, "data_in");
-	sc_trace(fp, next_instruction_adress_out_sig, "adress_out");
-	sc_trace(fp, instruction_data_out_sig, "data_out");
+	sc_trace(fp, opcode_out_sig, "opcode_out");
+	sc_trace(fp, rs_out_sig, "rs_out");
+	sc_trace(fp, rt_out_sig, "rt_out");
+	sc_trace(fp, rd_out_sig, "rd_out");
+	sc_trace(fp, rdShamtFunct_out_sig, "rdShamtFunct_out");
 	//========================= 
 
 	sc_start();
--- trab1/tests/buffers/buffer1_testbench.h
+++ trab1/tests/buffers/buffer1_testbench.h
@@ -7,32 +7,27 @@
 SC_MODULE(buffer1_testbench){
     sc_in<bool> Clk;
 
-    sc_out<sc_uint<5>> next_instruction_adress_in;
     sc_out<sc_uint<32>> instruction_data_in;
 
     void TbGen() {
 
         wait(); 
 
-        next_instruction_adress_in.write(12);
         instruction_data_in.write(0b10011100100101001010010110111111);
 
 
         wait(); 
 
-        next_instruction_adress_in.write(24);
         instruction_data_in.write(14);
 
 
         wait();
 
-        next_instruction_adress_in.write(5);
         instruction_data_in.write(2);
 
         
         wait(); 
 
-        next_instruction_adress_in.write(1);
         instruction_data_in.write(1);
     
         wait();
